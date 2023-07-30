b7eef504174b7fe570bec3b171fbe309c4256b72
Marcus Andrist <marcus.andrist@drake.edu>
2023-07-30 12:46:12 -0500

    Testing environment

--- server/app.py
+++ server/app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
